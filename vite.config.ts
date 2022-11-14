7fcb3d2542b0ac3e23e2c7f630540eb9afca49c5
David Dikman <ddikman@gmail.com>
2022-11-14 23:05:54 +0900

    Updated required libs and added pre-rendering to index page

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { ssr } from 'vite-plugin-ssr/plugin' // ADDED
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    ssr({ prerender: true }) // ADDED
+  ]
 })
