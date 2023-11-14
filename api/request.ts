9fb221bcedc4ce9bb105b9e9c4b1a9bb42872ef1
David Dikman <ddikman@gmail.com>
2023-11-14 18:09:01 +0900

    Added so that word requests are stored to firestore as well

--- api/request.ts
+++ api/request.ts
@@ -1,4 +1,16 @@
 import { WebClient } from '@slack/web-api'
+import { initializeApp } from 'firebase/app';
+import { getFirestore, doc, setDoc } from 'firebase/firestore';
+
+const app = initializeApp(JSON.parse(process.env.FIREBASE_CONFIG_JSON as string));
+const db = getFirestore(app);
+
+async function saveRequest(word: string) {
+  await setDoc(doc(db, 'genre-substantif/data/word-requests/' + word), {
+    word: word,
+    timestamp: Date.now()
+  })
+}
 
 async function sendRequest(word: string) {
     const token = process.env.SLACK_TOKEN
@@ -11,9 +23,9 @@ async function sendRequest(word: string) {
 export default async function handler(request: any, response: any) {
     const { word: word } = request.query;
     await sendRequest(word)
+    await saveRequest(word)
     const message = `OK. Requesting '${word}' to be added.`
     console.log(message)
     response.setHeader('Access-Control-Allow-Origin', '*')
     return response.end(message);
   }
-  
